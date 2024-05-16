cd31368fd17c8738424808425339acbfbdf13534
maru <toshidai.tarou11111@gmail.com>
2024-05-16 20:14:23 +0900

    開始演出を追加

--- SDPBL(2)_1/Game.h
+++ SDPBL(2)_1/Game.h
@@ -14,5 +14,31 @@ public:
 private:
 	//ユニークポインタの配列を作成
 	Array<std::unique_ptr<BaseWindow>>windows;
+	//開始演出が終わったかどうか
+	bool flagStart = false;
+	//ゲーム開始時の処理をするためのフラグ
+	bool flagS = false;
+	//カウントダウン開始時の処理をするためのフラグ
+	bool flagC = false;
+	//開始演出の時間
+	Stopwatch stopwatch;
+	//背景サイズ
+	double scale = 0.5;
+	//スタート位置
+	Vec2 fromP ={Scene::Size().x / 2, Scene::Size().y / 2};
+	Vec2 fromS = { 0.5,0 };
+	//ゴール位置
+	Vec2 toP = { Scene::Size().x / 2,Scene::Size().y / 2 + 98 };
+	Vec2 toS = {0.742,0};
+	// 移動の割合 0.0～1.0
+	double t;
+	// イージング関数を適用
+	double e;
+	// スタート位置からゴール位置へ e の割合だけ進んだ位置
+	Vec2 pos = fromP;
+	//移動終了フラグ
+	bool flagDoneMove = false;;
+	//ゲームオーバーフラグ
+	bool flagEnd = false;
 };
 
--- SDPBL(2)_1/Title.cpp
+++ SDPBL(2)_1/Title.cpp
@@ -9,10 +9,10 @@ Title::Title(const InitData& init)
 
 void Title::update() {
 	if (KeyEnter.down()) {
-		changeScene(State::Game,0);
+		changeScene(State::Game,0s);
 	}
 }
 
 void Title::draw()const {
-
+	TextureAsset(U"BackGround").scaled(0.5).drawAt(Scene::Size().x / 2, Scene::Size().y / 2);;
 }
