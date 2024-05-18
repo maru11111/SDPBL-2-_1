8015eb8a681b24792d7d23f75649032fec19e4b0
maru <toshidai.tarou11111@gmail.com>
2024-05-18 20:13:09 +0900

    タイマーリセット、ゲームオーバー用メソッド、広告画像の描画を実装

--- SDPBL(2)_1/BaseWindow.cpp
+++ SDPBL(2)_1/BaseWindow.cpp
@@ -6,6 +6,10 @@ BaseWindow::BaseWindow(Vec2 windowPos, Vec2 windowSize, double batuSize, double 
 	m_startTime(startTime)
 {}
 
+void BaseWindow::timeReset() {
+	m_time.reset();
+}
+
 void BaseWindow::timeStart() {
 	m_time.start();
 }
@@ -58,39 +62,114 @@ void BaseWindow::actAdClick() {
 	//未実装
 }
 
+void BaseWindow::setPopKind() {
+	if (m_beClickAd == true) {
+		kindPop = kind;
+	}
+}
+
+AdKind BaseWindow::getKindPop() {
+	return kindPop;
+}
+
 void BaseWindow::update(double dt) {
-	ClearPrint();
+	//Print << U"m_startTime=" << m_startTime;
+	
 	if (m_startTime < m_time.sF()) {
 
 		isClick();
 		isClickAd();
+		setPopKind();
 		act();
 		move(dt);
 
 	}
-	Print << m_AllBatuNotClicked;
-	Print << m_time;
-
+	//Print << m_AllBatuNotClicked;
+	//Print << m_time;
+	//Print << (int)AdKind();
 }
 
 void BaseWindow::draw() {
+	//Print << U"m_startTime < m_time.sF()=" << (m_startTime < m_time.sF());
+	//Print << m_startTime;
+	//Print << m_time.sF();
 	if (m_startTime < m_time.sF()) {
-
+		
 		switch (kind) {
 		case AdKind::Anna:
-			m_window.scaled(m_size)(v).draw(ColorF{ 1,1,1,m_opacity });
+			m_window.scaled(m_size)(TextureAsset(U"Anna")).draw(ColorF{1,1,1,m_opacity});
 			break;
 		case AdKind::Azarashi:
-			m_window.scaled(m_size)(s).draw(ColorF{ 1,1,1,m_opacity });
+			m_window.scaled(m_size)(TextureAsset(U"Azarashi")).draw(ColorF{1,1,1,m_opacity});
+			break;
+		case AdKind::BatuDarake:
+			m_window.scaled(m_size)(TextureAsset(U"BatuDarake")).draw(ColorF{ 1,1,1,m_opacity });
+			break;
+		case AdKind::Building:
+			m_window.scaled(m_size)(TextureAsset(U"Building")).draw(ColorF{ 1,1,1,m_opacity });
+			break;
+		case AdKind::Car:
+			m_window.scaled(m_size)(TextureAsset(U"Car")).draw(ColorF{ 1,1,1,m_opacity });
+			break;
+		case AdKind::Christmas:
+			christmas.advance();
+			m_window.scaled(m_size)(christmas).draw(ColorF{ 1,1,1,m_opacity });
+			break;
+		case AdKind::Hair:
+			m_window.scaled(m_size)(TextureAsset(U"Hair")).draw(ColorF{ 1,1,1,m_opacity });
+			break;
+		case AdKind::MatchingApp:
+			m_window.scaled(m_size)(TextureAsset(U"MatchingApp")).draw(ColorF{ 1,1,1,m_opacity });
+			break;
+		case AdKind::QuickCatch:
+			m_window.scaled(m_size)(TextureAsset(U"QuickCatch")).draw(ColorF{ 1,1,1,m_opacity });
 			break;
 		default:
-
 			break;
 		}
 		m_batu.scaled(m_size).draw(ColorF{ 1,0,0,m_opacity });
 	}
+}
 
+void BaseWindow::drawPop(double dt) {
+	//m_opacityP += 1*dt;
+	m_opacityP = 1;
+	switch (kindPop) {
+	case AdKind::Anna:
+		TextureAsset(U"Anna").scaled(1).drawAt(Scene::Center(), ColorF(1, 1, 1, m_opacityP));
+		break;
+	case AdKind::Azarashi:
+		TextureAsset(U"Azarashi").scaled(0.44).drawAt(Scene::Center() + Vec2(0, 33), ColorF(1, 1, 1, m_opacityP));
+		break;
+	case AdKind::BatuDarake:
+		TextureAsset(U"BatuDarake").scaled(0.28).drawAt(Scene::Center() + Vec2(0, 34), ColorF(1, 1, 1, m_opacityP));
+		break;
+	case AdKind::Building:
+		TextureAsset(U"Building").scaled(0.195).drawAt(Scene::Center() + Vec2(0, 34), ColorF(1, 1, 1, m_opacityP));
+		break;
+	case AdKind::Car:
+		TextureAsset(U"Car").scaled(0.505).drawAt(Scene::Center() + Vec2(0, 34), ColorF(1, 1, 1, m_opacityP));
+		break;
+	case AdKind::Christmas:
+		/*VideoTextureAssetをstaticにできないためGameで描画しています...*/
+		//christmas.advance();
+		//christmas.drawAt(Scene::Center());
+		break;
+	case AdKind::Hair:
+		TextureAsset(U"Hair").scaled(0.57).drawAt(Scene::Center() + Vec2(0, 34), ColorF(1, 1, 1, m_opacityP));
+		break;
+	case AdKind::MatchingApp:
+		TextureAsset(U"MatchingApp").scaled(0.305).drawAt(Scene::Center() + Vec2(0, 34), ColorF(1, 1, 1, m_opacityP));
+		break;
+	case AdKind::QuickCatch:
+		TextureAsset(U"QuickCatch").scaled(0.97).drawAt(Scene::Center() + Vec2(0, 35), ColorF(1, 1, 1, m_opacityP));
+		break;
+	default:
+		break;
+	}
 }
 
 Stopwatch BaseWindow::m_time;
 bool BaseWindow::m_AllBatuNotClicked = true;
+AdKind BaseWindow::kindPop=AdKind::Anna;
+double BaseWindow::m_opacityP=0;
