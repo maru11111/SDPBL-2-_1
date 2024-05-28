c61c6cc927c919cea6e51e4b746cde40be646b61
maru <toshidai.tarou11111@gmail.com>
2024-05-28 14:38:59 +0900

    ウィンドウタイトルを変更

--- SDPBL(2)_1/Main.cpp
+++ SDPBL(2)_1/Main.cpp
@@ -4,35 +4,38 @@
 
 void Main()
 {
+	TextureAsset::Register(U"BackGround", Resource(U"image/haikei.png"));
+	TextureAsset::Register(U"Anna", Resource(U"image/SDPBL広告.png"));
+	TextureAsset::Register(U"Azarashi", Resource(U"image/azarashi.png"));
+	TextureAsset::Register(U"BatuDarake", Resource(U"image/batu.png"));
+	TextureAsset::Register(U"Building", Resource(U"image/building.png"));
+	TextureAsset::Register(U"Car", Resource(U"image/car.png"));
+	//VIdeoTextureAssetがないためそれぞれのクラスが動画ファイルを持ってます
+	//VideoTextureAsset::Register(U"Christmas", Resource(U"image/christmas.mp4"));
+	TextureAsset::Register(U"Hair", Resource(U"image/hair.jpg"));
+	TextureAsset::Register(U"MatchingApp", Resource(U"image/matching.png"));
+	TextureAsset::Register(U"QuickCatch", Resource(U"image/quick.png"));
+	TextureAsset::Register(U"Title", Resource(U"image/title7.png"));
 
-	TextureAsset::Register(U"BackGround", U"image/haikei.png");
-	TextureAsset::Register(U"Anna", U"image/SDPBL広告.png");
-	TextureAsset::Register(U"Azarashi", U"image/azarashi.png");
-	TextureAsset::Register(U"BatuDarake", U"image/batu.png");
-	TextureAsset::Register(U"Building", U"image/building.png");
-	TextureAsset::Register(U"Car", U"image/car.png");
-	//TextureAsset::Register(U"Christmas", U"image/christmas.mp4");
-	TextureAsset::Register(U"Hair", U"image/hair.jpg");
-	TextureAsset::Register(U"MatchingApp", U"image/matching.png");
-	TextureAsset::Register(U"QuickCatch", U"image/quick.png");
-	TextureAsset::Register(U"Title", U"image/title7.png");
+	FontAsset::Register(U"CountDown", 100, Resource(U"font/keifont.ttf"));
+	FontAsset::Register(U"Title", 75, Resource(U"font/keifont.ttf"));
+	FontAsset::Register(U"Skip", 50, Resource(U"font/keifont.ttf"));
+	FontAsset::Register(U"GameOver", 100, Resource(U"font/keifont.ttf"));
+	FontAsset::Register(U"Num",50, Resource(U"font/keifont.ttf"));
+	FontAsset::Register(U"Clear", 100, Resource(U"font/keifont.ttf"));
+	FontAsset::Register(U"Stage", 50, Resource(U"font/keifont.ttf"));
+	FontAsset::Register(U"StageDS", 40, Resource(U"font/keifont.ttf"));
 
-	FontAsset::Register(U"CountDown", 100, U"font/keifont.ttf");
-	FontAsset::Register(U"Title", 75, U"font/keifont.ttf");
-	FontAsset::Register(U"Skip", 50, U"font/keifont.ttf");
-	FontAsset::Register(U"GameOver", 100, U"font/keifont.ttf");
-	FontAsset::Register(U"Num",50, U"font/keifont.ttf");
-	FontAsset::Register(U"Clear", 100, U"font/keifont.ttf");
-	FontAsset::Register(U"Stage", 50, U"font/keifont.ttf");
-	FontAsset::Register(U"StageDS", 40, U"font/keifont.ttf");
+	AudioAsset::Register(U"CountDown", Resource(U"audio/ピッピッピー.wav"));
+	AudioAsset::Register(U"PlayBGM", Resource(U"audio/大慌て.mp3"), Loop::Yes);
+	AudioAsset::Register(U"Hit", Resource(U"audio/damage.mp3"));
+	AudioAsset::Register(U"GameOver", Resource(U"audio/GameOver.wav"));
+	AudioAsset::Register(U"BatuClick", Resource(U"audio/8bit獲得1.mp3"));
+	AudioAsset::Register(U"BatuClickLast", Resource(U"audio/8bit獲得6.mp3"));
+	AudioAsset::Register(U"Clear", Resource(U"audio/ジャジャーン3.mp3"));
 
-	AudioAsset::Register(U"CountDown", U"audio/ピッピッピー.wav");
-	AudioAsset::Register(U"PlayBGM", U"audio/大慌て.mp3", Loop::Yes);
-	AudioAsset::Register(U"Hit", U"audio/damage.mp3");
-	AudioAsset::Register(U"GameOver", U"audio/GameOver.wav");
-	AudioAsset::Register(U"BatuClick", U"audio/8bit獲得1.mp3");
-	AudioAsset::Register(U"BatuClickLast", U"audio/8bit獲得6.mp3");
-	AudioAsset::Register(U"Clear", U"audio/ジャジャーン3.mp3");
+	//ウィンドウタイトルを変更
+	Window::SetTitle(U"広告のバツを押すゲーム");
 
 	App manager;
 	manager.add<Title>(State::Title);
