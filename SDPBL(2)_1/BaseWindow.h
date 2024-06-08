160a807cf4ad6725c933a366643c568426c69dca
maru <toshidai.tarou11111@gmail.com>
2024-06-08 18:42:50 +0900

    annaの広告が表示されるように変更

--- SDPBL(2)_1/BaseWindow.h
+++ SDPBL(2)_1/BaseWindow.h
@@ -97,7 +97,7 @@ protected:
 	//スイッチ
 	int m_flagS = 0;
 	//広告の種類
-	AdKind kind = (AdKind)Random(1,8);
+	AdKind kind = (AdKind)Random(0,0);
 	//踏んでしまった広告の種類
 	static AdKind kindPop;
 	//踏んでしまった広告の不透明度
